67c71679f915cdce78ea1afdf74913965f2c09ac
LrkDev <20665427+LrkDev@users.noreply.github.com>
2022-09-14 02:56:05 -0400

    add dev task to compile .css add build task to compile .css and .min.css add watch task to compile on change

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const prefix = require('gulp-autoprefixer');
-const minify = require('gulp-clean-css');
-const rename = require('gulp-rename');
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const prefix = require('gulp-autoprefixer')
+const minify = require('gulp-clean-css')
+const rename = require('gulp-rename')
 
-gulp.task('compilescss', function() {
-    gulp.src('./piment/**/*.scss')
+gulp.task('dev', function () {
+    return gulp
+        .src('./piment/**/*.scss')
         .pipe(sass())
         .pipe(prefix())
+        .pipe(gulp.dest('public/assets/css')) // .css
+})
+
+gulp.task('build', function () {
+    return gulp
+        .src('./piment/**/*.scss')
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(gulp.dest('public/assets/css')) // .css
         .pipe(minify())
-        .pipe(rename(function (path) {
-            return {
-            dirname: path.dirname + "",
-            basename: path.basename + ".min",
-            extname: ".css"
-            };
-        }))
-        .pipe(gulp.dest('./public/assets/css'))
-});
+        .pipe(
+            rename(function (path) {
+                return {
+                    dirname: path.dirname + '',
+                    basename: path.basename + '.min',
+                    extname: '.css',
+                }
+            })
+        )
+        .pipe(gulp.dest('public/assets/css')) // .min.css
+})
+
+gulp.task('watch', function () {
+    gulp.watch(['piment/**/*.scss'], gulp.series('dev'))
+})
